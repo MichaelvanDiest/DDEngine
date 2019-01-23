8d854bdc04e776e835e1ac0b48f622eea570d352
Michael van Diest <michaelvandiest@gmail.com>
2019-01-22 20:18:31 -0500

    Updated AnimationManager.cs and Animation

--- DDAnimation.cs
+++ DDAnimation.cs
@@ -10,39 +10,39 @@ namespace DDEngine
         /// <summary>
         /// Sprite Sheet
         /// </summary>
-        Texture2D spriteStrip;
+        public Texture2D spriteStrip;
         /// <summary>
         /// Sprite scale.
         /// </summary>
-        float scale;
+        public float scale;
         /// <summary>
         /// Time since frame has been updated
         /// </summary>
-        int elapsedTime;
+        public int elapsedTime;
         /// <summary>
         /// Time a frame is displayed
         /// </summary>
-        int frameTime;
+        public int frameSpeed;
         /// <summary>
         /// Total number of frames
         /// </summary>
-        int frameCount;
+        public int frameCount;
         /// <summary>
         /// Index of the current frame we are displaying
         /// </summary>
-        int currentFrame;
+        public int currentFrame;
         /// <summary>
         /// Color of the frame
         /// </summary>
-        Color color;
+        public Color color;
         /// <summary>
         /// Area of the frame to display
         /// </summary>
-        Rectangle sourceRect = new Rectangle();
+        public Rectangle sourceRect = new Rectangle();
         /// <summary>
         /// Area to display the image in game
         /// </summary>
-        Rectangle destinationRect = new Rectangle();
+        public Rectangle destinationRect = new Rectangle();
         /// <summary>
         /// Width of the frame
         /// </summary>
@@ -59,16 +59,18 @@ namespace DDEngine
         /// If the animation loops
         /// </summary>
         public bool Looping;
-
+		/// <summary>
+		/// Position to draw the sprite
+		/// </summary>
         public Vector2 Position;
 
-        public DDAnimation(Texture2D texture, Vector2 position, int frameWidth, int frameHeight, int frameCount, int frameTime, Color color, float scale, bool looping)
+        public DDAnimation(Texture2D texture, Vector2 position, int frameWidth, int frameHeight, int frameCount, int frameSpeed, Color color, float scale, bool looping)
         {
             this.color = color;
             this.FrameWidth = frameWidth;
             this.FrameHeight = frameHeight;
             this.frameCount = frameCount;
-            this.frameTime = frameTime;
+            this.frameSpeed = frameSpeed;
             this.Looping = looping;
             this.Position = position;
             this.spriteStrip = texture;
@@ -78,34 +80,6 @@ namespace DDEngine
             currentFrame = 0;
 
 			Active = true;
-        }
-
-        public void Update(GameTime gameTime)
-        {
-			if (!Active) return;
-
-			//Update elapsed time
-			elapsedTime += (int)gameTime.ElapsedGameTime.TotalMilliseconds;
-
-			//Switch frames
-			if (elapsedTime > frameTime)
-			{
-				currentFrame++;
-
-				if (!Looping) Active = false;
-			}
-
-			//Reset elapsed time
-			elapsedTime = 0;
-			
-			//Grab the frame from the strip
-			sourceRect = new Rectangle(currentFrame * FrameWidth, 0, FrameWidth, FrameHeight);
-			destinationRect = new Rectangle((int)Position.X - (int)(FrameWidth * scale) / 2, (int)Position.Y - (int)(FrameHeight * scale) / 2, (int)(FrameWidth * scale), (int)(FrameHeight * scale));
-        }
-
-        public void Draw(SpriteBatch spriteBatch)
-        {
-			if (Active) spriteBatch.Draw(spriteStrip, destinationRect, sourceRect, color);
         }
     }
 }
---
+++ DDAnimationManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace DDEngine.DDEngine
+{
+	class DDAnimationManager
+	{
+		private DDAnimation curAnimation;
+
+		public void Update(GameTime gameTime)
+		{
+			if (!curAnimation.Active) return;
+
+			//Update elapsed time
+			curAnimation.elapsedTime += (int)gameTime.ElapsedGameTime.TotalMilliseconds;
+
+			//Switch frames
+			if (curAnimation.elapsedTime > curAnimation.frameSpeed)
+			{
+				curAnimation.currentFrame++;
+
+				//If the current frame is equal to or bigger the frame count then reset the animation
+				if (curAnimation.currentFrame >= curAnimation.frameCount)
+				{
+					curAnimation.currentFrame = 0;
+
+					if (!curAnimation.Looping) curAnimation.Active = false;
+				}
+
+				//Reset elapsed time
+				curAnimation.elapsedTime = 0;
+			}
+
+			//Grab the frame from the strip
+			curAnimation.sourceRect = new Rectangle(curAnimation.currentFrame * curAnimation.FrameWidth, 0, curAnimation.FrameWidth, curAnimation.FrameHeight);
+			curAnimation.destinationRect = new Rectangle((int)curAnimation.Position.X - (int)(curAnimation.FrameWidth * curAnimation.scale) / 2, (int)curAnimation.Position.Y - (int)(curAnimation.FrameHeight * curAnimation.scale) / 2, (int)(curAnimation.FrameWidth * curAnimation.scale), (int)(curAnimation.FrameHeight * curAnimation.scale));
+		}
+
+		public void Draw(SpriteBatch spriteBatch)
+		{
+			if (curAnimation.Active) spriteBatch.Draw(curAnimation.spriteStrip, curAnimation.destinationRect, curAnimation.sourceRect, curAnimation.color);
+		}
+
+		public void Add()
+		{
+			
+		}
+	}
+}
